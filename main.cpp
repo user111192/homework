04fa1fb802aa2a3c0729c7c5d6ed86a0355a231c
chenziang <3538995003@qq.com>
2022-12-17 14:52:15 +0800

    commit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 // Import namespace 'std'
 using namespace std;
 
-int a();
+
 
 // Main Function
 int main(int argc, char **argv) {
@@ -19,29 +19,14 @@ int main(int argc, char **argv) {
      * @param argc int
      * Argument Numbers
      * @param argv char[][]
-     * A 2D Array (char)
+     * 2-D Array (char)
      * @return Exit code
      */
-    // 读入数组
-    int n, m;
-    cin >> n >> m;
-    int arr[n+1][m+1];
 
-    for (int i=1;i<=n;i++) {
-        for (int j=1;j<=m;j++)
-            cin >> arr[i][j];
-    }
-    for (int i=2;i<=n;i++) {
-        for (int j=1;j<=m;j++){
-            if (arr[i-1][j] > arr[i][j]) {
-                cout << "No";
-                return 0;
-            }
-        }
-    }
-    cout << "Yes";
+
     return 0;
 }
+
 
 // Functions Definition
 
