64e422ab0779af70b5f7a88cac84259d4c0bd760
chenziang <3538995003@qq.com>
2022-12-24 10:25:22 +0800

    create fastutil.h -> main update CMakeLists.txt

---
+++ fastutil.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+// Vestibulum commodo. Ut rhoncus gravida arcu.
+
+//
+// Created by 陈子昂 on 2022/12/24.
+//
+
+#ifndef HOMEWORK_FASTUTIL_H
+#define HOMEWORK_FASTUTIL_H
+
+#define final const
+#define static_final static final
+
+#endif //HOMEWORK_FASTUTIL_H
