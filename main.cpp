86f87fcc3233b93aa8d3ee0389e74c7dacf615b3
chenziang <3538995003@qq.com>
2023-08-09 13:03:48 +0800

    commit
    Tested-by: chenziang <3538995003@qq.com>

--- main.cpp
+++ main.cpp
@@ -3,16 +3,36 @@
 // 你看头文件也是老师的写法.
 
 using namespace std;
-const int MAX = 5;
+const int MAX = 1005;
 typedef long long ll;
 const ll inf = 1ll << 60;
-
+#define qq(x) (weixin(x))
+#define weixin(x) (alipay(x))
+#define alipay(x) (pdd(x))
+#define pdd(x) ((x))
+int dp[MAX+1][MAX+1], n, m, v[MAX+1],w[MAX+1], ans;
 
 
 int main() {
+    scanf("%d%d", &n, &m);
+    for (int i=1;i<=n;i++) {
+        scanf("%d%d",v+i,w+i);
+    }
+    // 转移 dp[i][j] = max(dp[i-1][j], dp[i-1][j-v[i]]+w[i]);
+    memset(dp,128,sizeof(dp));
+    dp[0][0] = 0;
+    for (int i=1;i<=n;i++) {
+        for (int j=0;j<=m;j++) {
+            if (j < v[i])
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-v[i]]+w[i]);
+        }
+    }
+    for (int i=0;i<=m;i++)
+        ans = max(ans,dp[n][i]);
 
-
-
+    printf("%d\n",ans);
 
     // 这一段是输出程序运行时间, 提交到oj时要删掉
     // [-提交到oj时删除-
@@ -26,7 +46,7 @@ int main() {
 void print_memo_use() {
     double mem_used;
     FILE *ff = nullptr;
-    ll pid = 0;
+    int pid = 0;
 
     system("pgrep homework > xxs");
     ff = fopen("xxs", "r");
