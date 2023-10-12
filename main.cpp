49eb7dc83553c0c8ccabaaa9f550802cc4fdca88
chenziang <3538995003@qq.com>
2023-10-12 09:20:32 +0800

    commit
    Tested-by: chenziang <3538995003@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,67 +1,55 @@
-#include <bits/stdc++.h>
-//       ^^^^^^^^^^^^^^^
-// 你看头文件也是老师的写法.
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <cstdio>
+#include <queue>
+#include <cstring>
+#include <cassert>
+#include <algorithm>
 
 using namespace std;
-const int MAX = 105;
+
+const int MAX = 3010;
 typedef long long ll;
-const ll inf = 1ll << 60;
-#define qq(x) (weixin(x))
-#define weixin(x) (alipay(x))
-#define alipay(x) (pdd(x))
-#define pdd(x) ((x))
-int T;
-int x, y, d;
-
-int lcm(int a,int b){
-    return a/__gcd(a,b)*b;
-}
-
+int n, m, cnt;
+int edges[MAX+1][MAX+1];
 
 int main() {
-    scanf("%d", &T);
-    while (T--) {
-        d=0;
-        scanf("%d%d", &x, &y);
-        if (x==y) {
-            printf("0\n");
-            continue;
-        }
-        if (y%x==0) {
-            printf("%d\n", y/x);
-            continue;
-        }
-        if (x%y==0) {
-            printf("%d\n", x/y);
-            continue;
-        }
-        d += lcm(x,y)/x;
-        d += lcm(x,y)/y;
-        printf("%d\n", d);
+    ios::sync_with_stdio(false);
+    cin >> n >> m;
+    for (int i=1,u,v;i<=m;i++) {
+        cin >> u >> v;
+        edges[u][v] = 1;
     }
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {
+            if (!edges[i][j]) continue;
+            if (i==j) continue;
+            for (int k=1;k<=n;k++) {
+                if (!edges[j][k]) continue;
+                if (i==k || j==k) continue;
+                for (int l=1;l<=n;l++) {
+                    if (!edges[i][l]) continue;
+                    if (!edges[l][k]) continue;
+                    if (i==l || j==l || k==l) continue;
+                    if (i!=j && j!=k && k!=l && i!=k && j!=l && i!=l) {
+                        // a->b,b->c,a->d,d->c
+                        if (edges[i][j] && edges[j][k] && edges[i][l] && edges[l][k]) {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << cnt/2 << endl;
+    
 
     // 这一段是输出程序运行时间, 提交到oj时要删掉
     // [-提交到oj时删除-
     double t_used = (1.0 * clock()) / CLOCKS_PER_SEC;
-    //printf("Time: %lfs %s \n", t_used, (t_used < 1) ? "\33[32mOK\33[0m" : "\33[31mFAIL\33[0m");
-    //print_memo_use();
+    // printf("Time: %lfs %s \n", t_used, (t_used < 1) ? "\33[32mOK\33[0m" : "\33[31mFAIL\33[0m");
     // -]
     return 0;
-}
-
-void print_memo_use() {
-    double mem_used;
-    FILE *ff = nullptr;
-    int pid = 0;
-
-    system("pgrep homework > xxs");
-    ff = fopen("xxs", "r");
-    fscanf(ff, "%d", &pid);
-    char pidc[101]{};
-    sprintf(pidc, "top -pid %d -l 1 | tail -n 1 | awk '{prll $8}' > xt", pid);
-    system(pidc);
-    ff = fopen("xt", "r");
-    fscanf(ff, "%lf", &mem_used);
-    mem_used /= 1000.0;
-    printf("Memory: %lfMB %s\n", mem_used, (mem_used < 128) ? "\33[32mOK\33[0m" : "\33[31mFAIL\33[0m");
 }
