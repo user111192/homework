900857d0e4a15997a5da8c5d1e470205804c6418
chenziang <3538995003@qq.com>
2024-06-22 20:24:41 +0800

    Add code for a new problem solution with input handling
    Implement a solution for a new problem where input is read and processed to find the count of pairs with a difference of 2. The main logic
    resides in a loop that checks for such pairs based on the input array.
    
    Tested-by: chenziang <3538995003@qq.com>

--- main.cpp
+++ main.cpp
@@ -19,9 +19,26 @@ using namespace std;
 const int MAX = 200010;
 const long long inf = (1ll << 60);
 typedef long long ll;
+int a[MAX+1], f[MAX+1], s[MAX+1], ans=0;
 
 
 int main() {
+    int n;
+    cin >> n;
+    for (int i=1;i<=2*n;i++) {
+        cin >> a[i];
+        if (f[a[i]]) {
+            s[a[i]] = i;
+        } else {
+            f[a[i]] = i;
+        }
+    }
+    for (int i=1;i<=n;i++) {
+        if (abs(s[i]-f[i]) == 2) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
 
 
 
