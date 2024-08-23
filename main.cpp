7b4af6ae18869d19bb3ccbcf7f5d1763d29115e6
chenziang <3538995003@qq.com>
2024-08-23 08:13:29 +0800

    feat(main): 使用map追踪歌曲顺序, 增加睡着时长计算
    移除之前的数组f和s用于追踪歌曲出现的开始和结束位置，现在使用map来直接映射歌曲名到其顺序。
    移除计算歌曲播放间隔以判断是否睡着的逻辑，改为输入最后一首听完的歌曲名，然后累加后续所有歌曲的播放时长，以计算睡着的总时长。
    
    Closes #5
    
    Signed-off-by:
    chenziang <3538995003@qq.com>

--- main.cpp
+++ main.cpp
@@ -19,29 +19,31 @@ using namespace std;
 const int MAX = 200010;
 const long long inf = (1ll << 60);
 typedef long long ll;
-int a[MAX+1], f[MAX+1], s[MAX+1], ans=0;
-
+map<string,int> mp;
+int a[MAX+1];
 
 int main() {
+    ios::sync_with_stdio(false);
     int n;
     cin >> n;
-    for (int i=1;i<=2*n;i++) {
-        cin >> a[i];
-        if (f[a[i]]) {
-            s[a[i]] = i;
-        } else {
-            f[a[i]] = i;
-        }
-    }
     for (int i=1;i<=n;i++) {
-        if (abs(s[i]-f[i]) == 2) {
-            ans++;
-        }
+        string s;
+        // 歌曲的名称, 播放时长
+        cin >> s >> a[i];
+        mp[s] = i;
+    }
+    // wowo听完这首歌后睡着
+    string s1;
+    cin >> s1;
+    int start = mp[s1]+1;
+    // 睡着时播放歌曲总时长
+    int ans = 0;
+    for (int i=start;i<=n;i++) {
+        ans += a[i];
     }
     cout << ans << endl;
 
 
-
-
     return 0;
 }
+
