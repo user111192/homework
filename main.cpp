aa6dcfb159b657e4589383844458ab6241c5dc14
chenziang <3538995003@qq.com>
2024-08-31 21:16:08 +0800

    feat: 增加疲劳度计算
    增加疲劳度计算的功能。
    
    BREAKING CHANGE: 旧版本功能被替换

--- main.cpp
+++ main.cpp
@@ -19,27 +19,27 @@ using namespace std;
 const int MAX = 200010;
 const long long inf = (1ll << 60);
 typedef long long ll;
-map<string,int> mp;
+int n;
 int a[MAX+1];
+char h[MAX+1];
+ll ans=inf;
 
 int main() {
     ios::sync_with_stdio(false);
-    int n;
     cin >> n;
     for (int i=1;i<=n;i++) {
-        string s;
-        // 歌曲的名称, 播放时长
-        cin >> s >> a[i];
-        mp[s] = i;
+        cin >> a[i] >> h[i];
     }
-    // wowo听完这首歌后睡着
-    string s1;
-    cin >> s1;
-    int start = mp[s1]+1;
-    // 睡着时播放歌曲总时长
-    int ans = 0;
-    for (int i=start;i<=n;i++) {
-        ans += a[i];
+    for (int i=1;i<=100;i++) {
+        for (int j=1;j<=100;j++) {
+            ll nans=0;
+            int lpos=i, rpos=j;
+            for (int k=1;k<=n;k++) {
+                if (h[k] == 'L') nans+=abs(lpos-a[k]), lpos=a[k];
+                if (h[k] == 'R') nans+=abs(rpos-a[k]), rpos=a[k];
+            }
+            ans=min(ans, nans);
+        }
     }
     cout << ans << endl;
 
